e55aa7d9b4a644439557ac1d472e21c434627ecf
vidovb <vidovb@taltech.ee>
2024-06-16 23:45:32 +0300

    Added collision with screen borders

--- badhrtp2real/Game1.cs
+++ badhrtp2real/Game1.cs
@@ -16,6 +16,9 @@ public class Game1 : Game
     private int characterX = 0;
     private int characterY = 0;
 
+    public static int CHARACTER_WIDTH = 32;
+    public static int CHARACTER_HEIGHT = 32;
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -28,6 +31,7 @@ public class Game1 : Game
         // TODO: Add your initialization logic here
         
         textureMap.Add("ball", Content.Load<Texture2D>("Red-Ball-PNG-Pic"));
+        Movement.setBounds(Window.ClientBounds.Width, Window.ClientBounds.Height);
 
         base.Initialize();
     }
@@ -45,11 +49,42 @@ public class Game1 : Game
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        if (Keyboard.GetState().IsKeyDown(Keys.Right)) characterX++;
-        if (Keyboard.GetState().IsKeyDown(Keys.Left)) characterX--;
+
+        if (Keyboard.GetState().IsKeyDown(Keys.Right))
+        {
+            int newPos = Movement.MoveRight(characterX);
+            if (!Movement.willCollide(newPos, characterY, Keys.Right))
+            {
+                characterX = newPos;
+            }
+        }
+
+        if (Keyboard.GetState().IsKeyDown(Keys.Left))
+        {
+            int newPos = Movement.MoveLeft(characterX);
+            if (!Movement.willCollide(newPos, characterY, Keys.Left))
+            {
+                characterX = newPos;
+            }
+        }
         // Y axis goes down, 0,0 is top left????
-        if (Keyboard.GetState().IsKeyDown(Keys.Up)) characterY--;
-        if (Keyboard.GetState().IsKeyDown(Keys.Down)) characterY++;
+        if (Keyboard.GetState().IsKeyDown(Keys.Up))
+        {
+            int newPos = Movement.MoveUp(characterY);
+            if (!Movement.willCollide(newPos, characterY, Keys.Up))
+            {
+                characterY = newPos;
+            }
+        }
+
+        if (Keyboard.GetState().IsKeyDown(Keys.Down))
+        {
+            int newPos = Movement.MoveDown(characterY);
+            if (!Movement.willCollide(newPos, characterY, Keys.Down))
+            {
+                characterY = newPos;
+            }
+        }
         
         Console.WriteLine(characterX + "," + characterY);
         
@@ -68,7 +103,7 @@ public class Game1 : Game
         _spriteBatch.Begin();
         
         // rectangle to make it small
-        Rectangle sizeRectangle = new Rectangle(characterX, characterY, 32, 32);
+        Rectangle sizeRectangle = new Rectangle(characterX, characterY, CHARACTER_WIDTH, CHARACTER_HEIGHT);
         _spriteBatch.Draw(textureMap["ball"], sizeRectangle, Color.White);
         _spriteBatch.End();
         
---
+++ badhrtp2real/Movement.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace badhrtp2real;
+
+public static class Movement
+{
+    private static int speed = 4;
+    private static int width;
+    private static int height;
+
+    public static void setBounds(int w, int h)
+    {
+        width = w;
+        height = h;
+    }
+
+    public static void SetSpeed(int newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+    public static int MoveRight(int currentX)
+    {
+        return currentX + speed;
+    }
+    
+    public static int MoveRight(int currentX, int diffSpeed)
+    {
+        return currentX + diffSpeed;
+    }
+    
+    public static int MoveLeft(int currentX)
+    {
+        return currentX - speed;
+    }
+    
+    public static int MoveLeft(int currentX, int diffSpeed)
+    {
+        return currentX - diffSpeed;
+    }
+    
+    public static int MoveUp(int currentY)
+    {
+        return currentY - speed;
+    }
+    
+    public static int MoveUp(int currentY, int diffSpeed)
+    {
+        return currentY - diffSpeed;
+    }
+    
+    public static int MoveDown(int currentY)
+    {
+        return currentY + speed;
+    }
+    
+    public static int MoveDown(int currentY, int diffSpeed)
+    {
+        return currentY + diffSpeed;
+    }
+
+    public static bool willCollide(int x, int y, Keys direction)
+    {
+        Console.WriteLine(width + "," + height);
+        
+        return direction switch 
+        {
+            Keys.Right => x + Game1.CHARACTER_WIDTH >= width,
+            Keys.Left => x <= 0,
+            Keys.Up => y <= 0,
+            Keys.Down => y + Game1.CHARACTER_HEIGHT >= height
+        };
+    }
+}
